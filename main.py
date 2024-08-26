992c7632bbd9d8dc8972d877832458c26bb4fdf1
Renil <renil@foxmail.com>
2024-08-26 11:25:21 +0800

    增加现在天气和按小时天气

--- main.py
+++ main.py
@@ -34,12 +34,12 @@ class ChatWeather(BasePlugin):
         pass
 
     @llm_func(name="access_the_weather")  # 设置函数名称
-    async def access_web(self, query, location: str, adm: str, time: int):
+    async def access_web(self, query, location: str, adm: str , time: str = "now"):
         """Call this function to get location-based weather.
-        - You can pass in the obtained weather time in the parameter "time", the optional values are: 3,7,10,15, if the user asks for weather that is not in here, select a longer phase and intercept the weather for the required time.
-        - The fxLink in result is Weather detail link, Only provide this link if the user needs more information.
+        - 您可以在参数“time”中传入获得的天气时间，可选值为:3d,7d,10d,15d，如果用户需要按小时的天气，可选值为24h,72h，如果需要现在天气，可选值只有now，如果用户要求的天气不是这些值，则选择较长的值并截取所需时间的天气。
+        - Only provide this fxLink if the user needs more information.
         - China's administrative divisions are organized into three levels. The first level includes "省", "自治区", and "直辖市". The second level comprises "自治州", "县", "自治县", and "市". The third level includes "乡", "民族乡", and "镇", which are subdivisions of "县" and "自治县".
-        - You need to identify the level of location zoning and use the higher level as 'adm' parameter.
+        - You need to use the higher level divisions as 'adm' parameter.
         - An example of partitioning parameters: INPUT is "山东省济南市历城区", "location" is "历城", "adm" is "山东",
         - Summary the json content result by yourself, DO NOT directly output anything in the result you got.
         - If return error, please tell the error reason.
@@ -47,15 +47,12 @@ class ChatWeather(BasePlugin):
         Args:
             location(str): location to get weather
             adm(str): The superior division of location, could be None
-            time(int): time to get
-    
+            time(str): time to get, could be now, 24h, 72h, 3d, 7d, 10d, 15d
+
         Returns:
             json: json result for the weather.
-            json: .fxLink is "详情链接"
-            json: .updateTime is "更新时间"
             json: the INT in .daily[INT] refers to the date from today, so .daily[0] is "今日天气".
-            json: .daily[INT].tempMax is "最高温度", .daily[INT].tempMin is "最低温度".
-            json: .daily[INT].textDay is "白天天气", .daily[0].textNight is "晚上天气".
+            json: the INT in .hourly[INT] refers to the hour from this hour + 1, so .hourly[0] is "一小时后天气".
         """
         import httpx
         import json
@@ -72,17 +69,20 @@ class ChatWeather(BasePlugin):
             locationdata = rlocation.json()
             if 'location' in locationdata and len(locationdata['location']) > 0:
                 locationid = locationdata['location'][0]['id']
+                self.ap.logger.debug("位置id获取成功：" + locationid)
             else:
-                print(locationdata)
+                self.ap.logger.error("调用失败，插件获取位置id失败：" + locationdata)
         else:
             return False
 
         params = {'location': locationid, 'key': self.hfkey}
-        rweather = httpx.get(self.hfapi + str(time) + 'd', params=params)
+        rweather = httpx.get(self.hfapi + time, params=params)
         weatherdata = '{}'
         if rweather.status_code == 200:
             weatherdict = rweather.json()
             weatherdata = json.dumps(weatherdict)
+        else:
+            self.ap.logger.error("调用失败，返回值" + str(rweather.status_code) + "，目前调用的api地址为：" + self.hfapi + time)
 
         return weatherdata
 
